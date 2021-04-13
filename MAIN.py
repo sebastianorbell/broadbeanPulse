c4dae596a241d24580f21b71809141a0e2a8fefe
triton5 <joe.hickie@hotmail.co.uk>
2021-04-13 12:05:05 +0100

    waveform improvements
    Signed-off-by: triton5 <joe.hickie@hotmail.co.uk>

---
+++ MAIN.py
@@ -0,0 +1,63 @@
+from qgor import Station
+from myPulse import RabiPulse
+from constructSequence import Sequencer, SequenceParamterClass, PulseBuilder
+import broadbean as bb
+import matplotlib.pyplot as plt
+
+try:
+    from broadbean.plotting import plotter
+except:
+    plotter = bb.plotter
+
+
+import numpy as np
+
+station = Station()
+awg = station.awg5014
+
+sequencer = Sequencer()
+
+chs = [1,2]
+origin = [0,0]
+primaryVector = np.array([1,1])
+rabi = RabiPulse(chs, sequencer, origin, primaryVector)
+
+measElem, _ = rabi.measureElem(10e-6)
+unloadElem, _ = rabi.unloadElem(20e-6, 1)
+varyElem, _ = rabi.varyElem(20e-6)
+
+seq = bb.Sequence()
+
+#seq.addElement(1, unloadElem)
+#seq.addElement(2, measElem)
+seq.addElement(1, varyElem)
+
+
+seq.setSR(rabi.sequencer.sampleRate)
+seq.checkConsistency()
+
+plotter(seq)
+plt.show()
+
+detunings = np.linspace(1.0, 2.5, 3)
+durations = np.linspace(10e-9, 500e-9, 3)
+new_seq = rabi.vary_base_sequence(seq, 'vary', primaryVector, detunings, durations)
+
+length = np.size(durations)*np.size(detunings)
+new_seq.addElement(length+1, measElem)
+new_seq.addElement(length+2, unloadElem)
+new_seq.setChannelAmplitude(1,4)
+new_seq.setChannelAmplitude(0,4)
+new_seq.setChannelOffset(1,0)
+new_seq.setChannelOffset(0,0)
+
+
+awg.save_and_load(*new_seq.outputForAWGFile(), 'joe.awg')
+
+order = [length+1, 'index', length+2]
+seqParam = SequenceParamterClass('Seq', new_seq, order)
+
+for i in range(length):
+    seqParam.set_raw(i+1)
+    plotter(new_seq)
+    plt.show()
--- myPulse.py
+++ myPulse.py
@@ -4,7 +4,7 @@ Created on 08/04/2021
 """
 import broadbean as bb
 from constructSequence import Sequencer, SequenceParamterClass, PulseBuilder
-
+from qgor import Station
 
 try:
     from broadbean.plotting import plotter
@@ -24,22 +24,32 @@ class RabiPulse(PulseBuilder):
         self.primaryVector = primaryVector
 
     def measureElem(self, duration):
-        elem, stop = sequencer.buildVectorElement(sequencer.buildSegmentJump, self.origin, duration, 0, self.chs, self.primaryVector, marker1=[0.0])
+        elem, stop = self.sequencer.buildVectorElement(self.sequencer.buildSegmentJump,
+                                                  self.origin,
+                                                  duration,
+                                                  0,
+                                                  self.chs,
+                                                  self.primaryVector,
+                                                  marker1=[0.0])
         return elem, stop
 
     def unloadElem(self, duration, amp):
-        elem, stop = sequencer.buildVectorElement(sequencer.buildSegmentJump, self.origin, duration, amp,
-                                                  self.chs, -self.primaryVector)
+        elem, stop = self.sequencer.buildVectorElement(self.sequencer.buildSegmentJump,
+                                                  self.origin,
+                                                  duration,
+                                                  amp,
+                                                  self.chs,
+                                                  -self.primaryVector)
         return elem, stop
 
     def varyElem(self, duration):
         bps = []
         stops = []
         for start, ch in zip(self.origin, self.chs):
-            bp, stop = sequencer.buildSegmentRamp(start, start, duration, name='vary{}ch'.format(ch))
+            bp, stop = self.sequencer.buildSegmentRamp(start, start, duration, name='vary{}ch'.format(ch))
             bps.append(bp)
             stops.append(stop)
-        elem = sequencer.buildElement(bps, self.chs)
+        elem = self.sequencer.buildElement(bps, self.chs)
 
         return elem, stop
 
@@ -83,53 +93,53 @@ class RabiPulse(PulseBuilder):
 
         return newseq
 
-bb_ramp = bb.PulseAtoms.ramp
-
-sequencer = Sequencer()
-bluep, stop = sequencer.buildSegmentRamp(0, 1, 10e-6, wait=10e-6)
-bluep2, stop = sequencer.buildSegmentJump(stop, 3, 10e-6, wait=10e-6)
-bluep3, stop = sequencer.buildSegmentJumpAndBack(stop, 2, 10e-6, wait=10e-6)
-
-bp = bluep + bluep2 + bluep3
-
-elem = sequencer.buildElement([bp], [1])
-
-plotter(elem)
-
-chs = [1,2]
-origin = [0,0]
-primaryVector = np.array([1,1])
-rabi = RabiPulse(chs, sequencer, origin, primaryVector)
-
-measElem, _ = rabi.measureElem(10e-6)
-unloadElem, _ = rabi.unloadElem(20e-6, 1)
-varyElem, _ = rabi.varyElem(20e-6)
-
-seq = bb.Sequence()
-
-#seq.addElement(1, unloadElem)
-#seq.addElement(2, measElem)
-seq.addElement(1, varyElem)
-
-
-seq.setSR(rabi.sequencer.sampleRate)
-seq.checkConsistency()
-
-plotter(seq)
-plt.show()
-
-detunings = np.linspace(1.0, 2.5, 3)
-durations = np.linspace(10e-9, 500e-9, 3)
-new_seq = rabi.vary_base_sequence(seq, 'vary', primaryVector, detunings, durations)
-
-length = np.size(durations)*np.size(detunings)
-new_seq.addElement(length+1, measElem)
-new_seq.addElement(length+2, unloadElem)
-
-order = [length+1, 'index', length+2]
-seqParam = SequenceParamterClass('Seq', new_seq, order)
-
-for i in range(length):
-    seqParam.set_raw(i+1)
-    plotter(new_seq)
-    plt.show()
+# bb_ramp = bb.PulseAtoms.ramp
+#
+# sequencer = Sequencer()
+# bluep, stop = sequencer.buildSegmentRamp(0, 1, 10e-6, wait=10e-6)
+# bluep2, stop = sequencer.buildSegmentJump(stop, 3, 10e-6, wait=10e-6)
+# bluep3, stop = sequencer.buildSegmentJumpAndBack(stop, 2, 10e-6, wait=10e-6)
+#
+# bp = bluep + bluep2 + bluep3
+#
+# elem = sequencer.buildElement([bp], [1])
+#
+# plotter(elem)
+#
+# chs = [1,2]
+# origin = [0,0]
+# primaryVector = np.array([1,1])
+# rabi = RabiPulse(chs, sequencer, origin, primaryVector)
+#
+# measElem, _ = rabi.measureElem(10e-6)
+# unloadElem, _ = rabi.unloadElem(20e-6, 1)
+# varyElem, _ = rabi.varyElem(20e-6)
+#
+# seq = bb.Sequence()
+#
+# #seq.addElement(1, unloadElem)
+# #seq.addElement(2, measElem)
+# seq.addElement(1, varyElem)
+#
+#
+# seq.setSR(rabi.sequencer.sampleRate)
+# seq.checkConsistency()
+#
+# plotter(seq)
+# plt.show()
+#
+# detunings = np.linspace(1.0, 2.5, 3)
+# durations = np.linspace(10e-9, 500e-9, 3)
+# new_seq = rabi.vary_base_sequence(seq, 'vary', primaryVector, detunings, durations)
+#
+# length = np.size(durations)*np.size(detunings)
+# new_seq.addElement(length+1, measElem)
+# new_seq.addElement(length+2, unloadElem)
+#
+# order = [length+1, 'index', length+2]
+# seqParam = SequenceParamterClass('Seq', new_seq, order)
+#
+# for i in range(length):
+#     seqParam.set_raw(i+1)
+#     plotter(new_seq)
+#     plt.show()
